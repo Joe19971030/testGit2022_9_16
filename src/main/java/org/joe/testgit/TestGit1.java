b7dd03ff4ac2fde0600c418a5dea5b234a624145
joe <1550298508@qq.com>
2022-09-16 10:55:42 +0800

    master commit4

--- src/main/java/org/joe/testgit/TestGit1.java
+++ src/main/java/org/joe/testgit/TestGit1.java
@@ -7,6 +7,7 @@ public class TestGit1 {
         System.out.println("TestGit2");
         System.out.println("TestGit3");
         System.out.println("TestGit4");
+        System.out.println("TestGit5master");
     }
 
 
